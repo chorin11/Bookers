945d8781bb3b5076c5da803e585f50c13c3a76bb
きむらあやか <ha.iy.r8ao@gmail.comgit config --global user.name chorin11>
2020-07-06 22:25:39 +0900

    [Add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :books
+
+  get 'top' => 'books#top'
+  root :to => 'books#top'
+  get 'books' => 'books#index'
 end
